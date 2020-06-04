851190b78f4b453bc4a97a58c6798e807e66be23
Сергей Вилков <you@example.com>
2020-06-04 12:46:21 +0300

    Js module construct

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,26 @@
-defaultTask = (cb) => {
-    console.log('ok, it`s es6 syntax');
-    cb();
-}
+// const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const folders = {
+    src: 'src/',
+    dist: 'dist/',
+    vendor: 'vendor/'
+};
+const { src, dist, vendor } = folders;
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-exports.default = defaultTask
+gulp.task('js', () => {
+    return gulp.src([`${src}**/*.js`, `!${vendor}*.js`])
+    .pipe(babel())
+    .pipe(gulp.src('vendor/**/*.js'))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js'}))
+    .pipe(gulp.dest(`${dist}`));
+});
+
+gulp.task('watch', () => {
+    gulp.watch(`${src}**/*.js`, gulp.parallel('js'));
+});
+
+gulp.task('default', gulp.series('watch'));
